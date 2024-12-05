8824bf0e34e2f86c7c7c30e773bee4b7ece83c79
ImFrankVS <fvictorios@alumno.ipn.mx>
2024-12-04 20:38:54 -0600

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://ImFrankVS.github.io',
+  base: 'Frank_CV',
 });
